ff0a42582be717cc36e94ab79ced8f83dc24f117
illia goncharov <goncharovphp@gmail.com>
2024-05-30 15:08:52 -0700

    first commit

---
+++ src/app/api/auth/_config/github.ts
@@ -0,0 +1,7 @@
+export default {
+  // clientId: process.env.GITHUB_ID || "Iv23ctZiQ592cQ3OcLi9",
+  // clientSecret:
+  //   process.env.GITHUB_SECRET || "3943f5ab1c33188c2a32fc8f567f07b894bf6b1d",
+  clientId: "Iv23ctZiQ592cQ3OcLi9",
+  clientSecret: "3943f5ab1c33188c2a32fc8f567f07b894bf6b1d",
+};
